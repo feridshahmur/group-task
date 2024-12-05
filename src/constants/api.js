c3550751f6bf12ac634a8201a9ca99ae6b367949
vusalasalimovaa <salimovavusala7@gmail.com>
2024-12-05 14:28:29 +0400

    first-commit

---
+++ src/constants/api.js
@@ -0,0 +1,10 @@
+const BASE_URL = "http://localhost:8080/";
+
+const endpoints = {
+  teachers: "teachers",
+  students: "students",
+  tasks: "tasks",
+};
+
+export { endpoints };
+export default BASE_URL;
---
+++ src/services/api/index.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import BASE_URL from "../../constants/api.js";
+
+//get all data
+async function getAllData(endpoint) {
+  try {
+    const response = await axios(`${BASE_URL}/${endpoint}`);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// get data by id
+async function getDataById(endpoint, id) {
+  try {
+    const response = await axios(`${BASE_URL}/${endpoint}/${id}`);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+// delete data by id
+async function deleteDataById(endpoint, id) {
+  try {
+    const response = await axios.delete(`${BASE_URL}/${endpoint}/${id}`);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+// add new data
+async function addNewData(endpoint, payload) {
+  try {
+    const response = await axios.post(`${BASE_URL}/${endpoint}`, payload);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+// edit data by id
+async function editDataById(endpoint, id, payload) {
+  try {
+    const response = await axios.put(`${BASE_URL}/${endpoint}/${id}`, payload);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export { getAllData, getDataById, deleteDataById, addNewData, editDataById };
