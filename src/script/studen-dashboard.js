0d9396760fdaf4373c6dea4fcd2e3c8f0e2456bb
NihadHesenli <mi7rtfggj@code.edu.az>
2024-12-05 19:31:49 +0400

    First commit display is kinda ready

--- src/script/studen-dashboard.js
+++ src/script/studen-dashboard.js
@@ -1,12 +1,20 @@
+//imports
+import BASE_URL from "../constants/api.js";
+import { endpoints } from "../constants/api.js";
+import { getAllData,getDataById,deleteDataById,addNewData,editDataById } from "../services/api/index.js";
+import axios, { Axios } from "axios";
+
+//responsive and other shits
 const mobileMenuButton = document.querySelector('.mobile-menu-button');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 
 function toggleMobileMenu() {
     sidebar.classList.toggle('translate-x-0');
-    overlay.classList.toggle('hidden');
-    setTimeout(() => overlay.classList.toggle('opacity-0'), 0);
-    document.body.style.overflow = sidebar.classList.contains('translate-x-0') ? 'hidden' : '';
+    sidebar.classList.toggle('z-40')
+    // overlay.classList.toggle('hidden');
+    // setTimeout(() => overlay.classList.toggle('opacity-0'), 0);
+    // document.body.style.overflow = sidebar.classList.contains('translate-x-0') ? 'hidden' : '';
 }
 
     mobileMenuButton.addEventListener('click', toggleMobileMenu);
@@ -25,3 +33,32 @@ const notificationIcon = document.querySelector('.material-icons-outlined:nth-ch
     notificationIcon.classList.add('scale-110');
     setTimeout(() => notificationIcon.classList.remove('scale-110'), 200);
 }, 5000);
+
+// task section
+
+const taskLists = document.querySelector('.all-tasks')
+async function getAllTasks() {
+     const res = await getAllData('tasks')
+     DrawCards(res.data)
+}
+
+function DrawCards(array) {
+    taskLists.innerHTML = ''
+    array.forEach(element => {
+        taskLists.innerHTML +=`
+        <div class="flex justify-around items-center text-gray-600 hover:text-indigo-800 py-4 transition-all duration-300 hover:translate-x-1 w-[340]">
+        <span class="material-icons-outlined mr-2">task</span>
+        <p class="w-44">
+        ${element.topic.charAt(0) + element.topic.slice(1).toLowerCase()}
+        </p>
+        <span class="ml-32">100/100</span>
+        <span class="material-icons-outlined ml-auto">keyboard_arrow_right</span>
+        </div>
+        ` 
+    });
+}
+getAllTasks()
+
+
+
+
